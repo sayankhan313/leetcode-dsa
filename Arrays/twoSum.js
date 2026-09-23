e6b755384e60d7a1fbefabc70d920b494e968e8a
ssk53 <ssk53@student.le.ac.uk>
2026-09-23 14:52:13 +0100

    two sum by hash map

--- Arrays/twoSum.js
+++ Arrays/twoSum.js
@@ -1,14 +1,32 @@
-var twoSum = function(nums, target) {
+// brute forse 
+// var twoSum = function(nums, target) {
     
-    for (i=0;i<nums.length;i++){
-        for (j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]=== target){
-                return [i,j]
+//     for (i=0;i<nums.length;i++){
+//         for (j=i+1;j<nums.length;j++){
+//             if(nums[i]+nums[j]=== target){
+//                 return [i,j]
                 
                 
-            }
-        }
+//             }
+//         }
         
-    }
+//     }
    
-};
+// };
+
+//hash map
+
+
+var twoSum = function(nums, target){
+    const map= new Map()
+    for (let i=0;i<nums.length;i++){
+        let first =nums[i];
+        let complement=target-first;
+        if(map.has(complement)){
+            return[map.get[complement],i]
+        }
+        map.set(nums[i],i)
+
+
+    }
+}
